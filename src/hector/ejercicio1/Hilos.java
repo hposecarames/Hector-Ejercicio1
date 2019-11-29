8aaa98af227b4ecabbbc9b3c2f1f4c4ebe59772d
DANIELCASTELAO\hposecarames <DANIELCASTELAO\hposecarames@perlanegra15.ad.danielcastelao.org>
2019-11-29 09:03:19 +0100

    Finalizado

--- src/hector/ejercicio1/Hilos.java
+++ src/hector/ejercicio1/Hilos.java
@@ -18,7 +18,7 @@ public class Hilos extends Thread {
     @Override
     public void run(){
         
-        for(int i =0;i<26;i++){
+        for(int i =1;i<26;i++){
             System.out.println(i+" "+this.getName());
             try {
                 sleep(500);
